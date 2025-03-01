bf665114a84cf63d9fdc16f73a5d73a41abde6c9
bijendra <bizen.manandhar@gmail.com>
2025-03-02 01:28:19 +0545

    chore: fix headers on send email to render api

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const config = {
     apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://simple-email-api-jyzt.onrender.com',
-    apiKey: process.env.NEXT_PUBLIC_API_KEY || '1234567890'
+    apiKey: process.env.NEXT_PUBLIC_API_KEY || 'e1e145c9-f0e1-4b28-91f1-726fedefc0e4'
   }
