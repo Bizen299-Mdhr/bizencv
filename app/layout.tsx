4f8ff281b8aa87f18d86796466b0089af195c8ef
bijendra <bizen.manandhar@gmail.com>
2025-03-09 14:10:56 +0545

    chore: update designs

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;600&display=swap" />
       </head>
       <body className={inter.className}>
         {children}
